057e8ece3899cabdd78f8928553de6bab87cfc5f
Vantuz <Shcherbina444@gmail.com>
2025-02-19 12:57:59 +0200

    Creating table

--- postgres-students.session.sql
+++ postgres-students.session.sql
@@ -1,3 +1,17 @@
-CREATE TABLE test();
+CREATE TABLE users_with_snake();
+DROP TABLE test ,users_with_snake, users;
 
 -- Как выполнить запрос ctrl + E(2)
+
+/* 
+
+char(5): '1234567' -> error, '12' -> '12   '
+varchar(5): '1234567' -> error, '12' -> '12'
+
+ */
+
+ CREATE TABLE users(
+    first_name varchar(64),
+    last_name varchar(64),
+    biography text
+ )
