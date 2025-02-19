d7d814081b7783c71ccac9e182df90a28fe2f39c
Vantuz <Shcherbina444@gmail.com>
2025-02-19 19:16:54 +0200

    Inserting data into a table

--- postgres-students.session.sql
+++ postgres-students.session.sql
@@ -1,17 +1,23 @@
-CREATE TABLE users_with_snake();
-DROP TABLE test ,users_with_snake, users;
+DROP TABLE users;
 
--- Как выполнить запрос ctrl + E(2)
+CREATE TABLE users(
+    first_name VARCHAR(64),
+    last_name VARCHAR(64),
+    biography text,
+    gender VARCHAR(30),
+    is_subscribe boolean,
+    birhday date,
+    foot_size smallint,
+    height numeric(5,2)
+);
 
-/* 
+INSERT INTO users VALUES
+('John','Doe','Tyt och mnogo texta OCH OCH OCH OCH OCH OCH OCH MNOGO','Male',true,'2003.04.25',43,1.80);
 
-char(5): '1234567' -> error, '12' -> '12   '
-varchar(5): '1234567' -> error, '12' -> '12'
+INSERT INTO users VALUES
+('Suzan','Doe','Tyt och mnogo texta OCH OCH OCH OCH OCH OCH OCH MNOGO','Female',true,'2003.07.12',40,1.70),
+('Henry','Doe','Tyt och mnogo texta OCH OCH OCH OCH OCH OCH OCH MNOGO','Male',false,'2002.01.25',45,1.90);
 
- */
 
- CREATE TABLE users(
-    first_name varchar(64),
-    last_name varchar(64),
-    biography text
- )
+
+INSERT INTO users VALUES('Blacke','Doe','Tyt och mnogo texta OCH OCH OCH OCH OCH OCH OCH MNOGO','Male',false,'2003.04.25',Null,null);
