1423e2d2af44fd9822f59023123ca7b6907dd5bc
Vantuz <Shcherbina444@gmail.com>
2025-02-22 20:24:34 +0200

    Alter table

--- postgres-students.session.sql
+++ postgres-students.session.sql
@@ -1,20 +1,43 @@
 --Ключ - ознака (чаще штучна) которая видризняе один рядок в таблице от другого
 -- Первинний ключ (PRIMARY KEY) - используестя для того, что бы ключу дать ограничение уникальности(UNIQUE) и ограничение NOT NULL
 
-/* первинные ключи на два столпца */
 
-DROP TABLE coordinats
+/* ALTER TABLE - Внесение изменений */
+DROP TABLE products;
 
-CREATE TABLE coordinats(
-    x INT,
-    y INT,
-    z INT,
-    CONSTRAINT "unique_coord" PRIMARY KEY(x,y,z)
-)
+CREATE TABLE products(
+    id serial PRIMARY KEY,
+    brand varchar(200) NOT NULL CHECK (brand != ''),
+    model varchar(300) NOT NULL CHECK(model != ''),
+    description text,
+    category varchar(200) NOT NULL,
+    price numeric(10,2) NOT NULL CHECK(price >0),
+    discounted_price numeric(10,2) CHECK (discounted_price <=price)
+);
 
--- (111,22,36);
--- (252, 76,88);
--- (111,22,36); <<---- Тут возвращается ошибка
 
-INSERT INTO coordinats VALUES(111,22,35),
-(252, 76,88)
+INSERT INTO products (brand,model,category,price) VALUES
+('IPHONE','15 PRO MAX SUPER PUPER MEGA GYGABYTE TERROBYTE HDMI EDITION','smartphones',201034.56),
+('Samsung','S55002231 SUPER ZOOM MEGA BLUM','smartphones', 1234.32),
+('Sony','A33222123','TV',500.88),
+('LG','GG322','TV',1488);
+
+
+ALTER TABLE products
+ADD CONSTRAINT "unique_brand_model_pair" UNIQUE(brand,model) ;
+
+
+INSERT INTO products (brand,model,category,price) VALUES
+('IPHONE','15 PRO MAX SUPER PUPER MEGA GYGABYTE TERROBYTE HDMI EDITION','smartphones',322.02);
+
+ALTER TABLE products
+ADD COLUMN quantity int;
+
+ALTER TABLE products
+ADD CONSTRAINT "products_quantity_check" CHECK(quantity >=0);
+
+ALTER TABLE products
+DROP CONSTRAINT "products_quantity_check";
+
+ALTER TABLE products
+DROP COLUMN quantity
