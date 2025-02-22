6dcfd66c31c5cb120f29e803dde5ec5344fdcb2f
Vantuz <Shcherbina444@gmail.com>
2025-02-22 19:01:13 +0200

    Composit primary kay

--- postgres-students.session.sql
+++ postgres-students.session.sql
@@ -1,25 +1,20 @@
-DROP TABLE messages
-
-CREATE TABLE messages(
-    id serial PRIMARY KEY,
-    body text NOT NULL CHECK (body !=''),
-    author varchar(256) NOT NULL CHECK (author !=''),
-    created_at timestamp DEFAULT current_timestamp,
-    is_read boolean DEFAULT false
-);
-
-INSERT INTO messages(author,body) VALUES('Hello JONATON','MEEEE');
-
-INSERT INTO messages (author,body) VALUES
-('Johnos','Hello BOBBA'),
-('Me','Go to GYM!'),
-('Johnos','OU YEEEESSSSSS!');
-
-INSERT INTO messages (author,body) VALUES
-('Peter','Hello'),
-('Peter','Hello');
-
-INSERT INTO messages VALUES(10,'Text mess','NoName');
-
 --Ключ - ознака (чаще штучна) которая видризняе один рядок в таблице от другого
 -- Первинний ключ (PRIMARY KEY) - используестя для того, что бы ключу дать ограничение уникальности(UNIQUE) и ограничение NOT NULL
+
+/* первинные ключи на два столпца */
+
+DROP TABLE coordinats
+
+CREATE TABLE coordinats(
+    x INT,
+    y INT,
+    z INT,
+    CONSTRAINT "unique_coord" PRIMARY KEY(x,y,z)
+)
+
+-- (111,22,36);
+-- (252, 76,88);
+-- (111,22,36); <<---- Тут возвращается ошибка
+
+INSERT INTO coordinats VALUES(111,22,35),
+(252, 76,88)
