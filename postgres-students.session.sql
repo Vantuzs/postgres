2c1bcca96dbcb8d3847a69d585aa596044964a69
Vantuz <Shcherbina444@gmail.com>
2025-02-23 11:53:17 +0200

    Books task resolve

--- postgres-students.session.sql
+++ postgres-students.session.sql
@@ -16,6 +16,32 @@ CREATE TABLE products(
 );
 
 
+CREATE TABLE books(
+    id serial PRIMARY KEY,
+    exzemplar int,
+    author varchar(100) NOT NULL CONSTRAINT author_need CHECK(author !=''),
+    book_name varchar(100) NOT NULL CONSTRAINT book_name_need CHECK(book_name !=''),
+    birthday date NOT NULL CONSTRAINT birthday_current_date CHECK(birthday <= current_date),
+    vidatniztvo varchar(200) CONSTRAINT vidatniztvo_need CHECK(vidatniztvo !=''),
+    category varchar(200) NOT NULL CONSTRAINT category_need CHECK(category !=''),
+    sinopsis varchar(200) NOT NULL CONSTRAINT sinopsis_need CHECK(sinopsis !=''),
+    book_status boolean NOT null
+)
+    --  PRIMARY KEY(author,book_name)
+
+DROP TABLE books
+
+INSERT INTO books(author,book_name,birthday,vidatniztvo,category,sinopsis,book_status) VALUES
+('SIGMA','SIGMA BOY LADN','2020.12.02','CHARIPORSKA','BOEVIK','serial',false)
+
+ALTER TABLE books
+ADD CONSTRAINT "exzemplar_by_need_positive" CHECK(exzemplar >0);
+
+ALTER TABLE books
+ADD CONSTRAINT "authod_name_books" UNIQUE(author,book_name)
+
+
+
 INSERT INTO products (brand,model,category,price) VALUES
 ('IPHONE','15 PRO MAX SUPER PUPER MEGA GYGABYTE TERROBYTE HDMI EDITION','smartphones',201034.56),
 ('Samsung','S55002231 SUPER ZOOM MEGA BLUM','smartphones', 1234.32),
