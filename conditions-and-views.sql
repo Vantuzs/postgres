6a854a68e8ea48d74ee6c1134e9e463e1a41ab32
Vantuz <Shcherbina444@gmail.com>
2025-03-12 16:10:36 +0200

    VIEW

--- conditions-and-views.sql
+++ conditions-and-views.sql
@@ -103,3 +103,61 @@ SELECT *,(
         ELSE 'sigma BOY'
     END
 ) FROM products
+
+
+
+
+------------------------------------------------------------------------------------------
+
+
+
+
+-- Высести инфу про всех юзеров + инфу сколько кто сделал заказов
+
+
+
+CREATE OR REPLACE VIEW  users_with_orders_amount AS (
+    SELECT u.id ,u.first_name, u.last_name, u.email , count(o.id) AS orders_amount FROM 
+    users AS u LEFT JOIN orders AS o
+    ON u.id = o.customer_id
+    GROUP BY u.id
+    ORDER BY "orders_amount"
+);
+
+SELECT * FROM users_with_orders_amount;
+
+
+-- Получить email тех юзеров, которые имеют мненьше 2 заказов
+
+SELECT email, orders_amount FROM users_with_orders_amount
+WHERE orders_amount<2;
+
+
+-- Можно удалять вьюхи
+
+DROP VIEW users_with_orders_amount
+
+
+
+--  Создать представление, которое сохраняет заказы с ихней стоимостью
+-- Добавим к этой вьюхе информацию про заказчика (id заказчика)
+
+CREATE OR REPLACE VIEW orders_with_price AS (
+    SELECT o.id,o.customer_id, sum(p.price*p.quantity) AS "order_sum", o.status FROM 
+  orders AS o JOIN orders_to_products AS otp
+  ON o.id = otp.order_id 
+  JOIN products AS p
+  ON p.id = otp.products_id
+  GROUP BY o.id
+);
+
+DROP VIEW orders_with_price;
+
+
+-- Вывсести юзеров с суммой денег, которую они потратили в нашем магазине
+
+SELECT u.id,u.email,sum(owp.order_sum) AS "sum" FROM 
+users AS u  LEFT JOIN orders_with_price AS owp
+ON u.id = owp.customer_id
+GROUP BY u.id
+ORDER BY sum
