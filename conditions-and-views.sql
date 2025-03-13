7363f4bc63794bd830cdf11b30b8a146950c0c90
Vantuz <Shcherbina444@gmail.com>
2025-03-13 16:54:20 +0200

    Materialized view

--- conditions-and-views.sql
+++ conditions-and-views.sql
@@ -203,3 +203,58 @@ SELECT add_numbers(10,5);
 -- Удаление функции
 
 DROP FUNCTION add_numbers(integer,integer);
+
+
+
+
+
+
+
+
+
+
+------------------------------------------------------------------
+
+/*
+
+Задача: порахувати кількість замовлень
+
+*/
+
+CREATE MATERIALIZED VIEW total_orders AS (
+    SELECT count(*) AS "загальна кількість замовлень" 
+    FROM orders
+);
+
+
+DROP MATERIALIZED VIEW total_orders;
+
+SELECT * FROM total_orders; -- 6956
+
+INSERT INTO orders (customer_id, status)
+VALUES (
+    6002,
+    false
+);
+
+REFRESH MATERIALIZED VIEW total_orders;
+
+DROP MATERIALIZED VIEW total_orders
+
+
+CREATE FUNCTION refresh_materialized_view()
+RETURNS void
+AS
+$$
+BEGIN
+    REFRESH MATERIALIZED VIEW total_orders;
+END;
+$$
+LANGUAGE plpgsql;
+
+DROP FUNCTION refresh_materialized_view();
+
+
+SELECT refresh_materialized_view();
+
+
